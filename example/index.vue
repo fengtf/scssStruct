484ae31420228defe12f5850cb9700d3a72338f1
fengtf <fengtf@ziroom.com>
2023-09-25 23:03:55 +0800

    feat: 增加example

---
+++ example/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="content">
+    <div class="name">腾飞</div>
+    <div class="dfgdf"></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.content {
+  .name {
+  }
+  .dfgdf {
+  }
+}
+</style>
